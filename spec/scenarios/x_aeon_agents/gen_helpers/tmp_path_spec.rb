c85e01d9a06aaee41cfb9207bc3c0d3c7c3e56b1
Test User <muriel@x-aeon.com>
2026-07-08 18:59:28 +0200

    test: update tmp_path spec to compare with absolute path
    The tmp_path helper now returns an absolute path, so the test expectation is updated to use File.expand_path.

--- spec/scenarios/x_aeon_agents/gen_helpers/tmp_path_spec.rb
+++ spec/scenarios/x_aeon_agents/gen_helpers/tmp_path_spec.rb
@@ -1,5 +1,5 @@
 describe XAeonAgents::GenHelpers, '#tmp_path' do
   it 'returns the default temporary folder path for agents' do
-    expect(process_erb('<%= tmp_path %>')).to eq '.x_aeon_agents_test/data/tmp'
+    expect(process_erb('<%= tmp_path %>')).to eq File.expand_path('.x_aeon_agents_test/data/tmp')
   end
 end
